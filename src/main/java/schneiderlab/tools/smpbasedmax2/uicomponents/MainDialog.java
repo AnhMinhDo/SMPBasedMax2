e11a6cc2aa71494d955fa6a8c6e7847284e46f11
anhminh <ahd.minh@gmail.com>
2025-06-10 14:26:29 +0200

    feat: main window of Application

---
+++ src/main/java/schneiderlab/tools/smpbasedmax2/uicomponents/MainDialog.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import javax.swing.*;
+import com.formdev.flatlaf.extras.*;
+import net.miginfocom.swing.*;
+/*
+ * Created by JFormDesigner on Mon Jun 09 23:41:48 CEST 2025
+ */
+
+
+
+/**
+ * @author anhminh
+ */
+public class MainDialog extends JFrame {
+	public MainDialog() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+		// Generated using JFormDesigner Educational license - Anh Minh Do
+
+		//======== this ========
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new MigLayout(
+			"hidemode 3,align center top",
+			// columns
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]" +
+			"[fill]",
+			// rows
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]" +
+			"[]"));
+		pack();
+		setLocationRelativeTo(getOwner());
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+	// Generated using JFormDesigner Educational license - Anh Minh Do
+	// JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
