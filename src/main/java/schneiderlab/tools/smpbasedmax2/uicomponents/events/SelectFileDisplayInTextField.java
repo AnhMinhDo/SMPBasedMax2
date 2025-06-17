ee9e8bac509ae0df83f907b655f5a1849b60a778
anhminh <ahd.minh@gmail.com>
2025-06-17 16:55:52 +0200

    feat: action event to open file choserDialog and if successful, add full file path to the text field

---
+++ src/main/java/schneiderlab/tools/smpbasedmax2/uicomponents/events/SelectFileDisplayInTextField.java
@@ -0,0 +1,36 @@
+package schneiderlab.tools.smpbasedmax2.uicomponents.events;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class SelectFileDisplayInTextField implements ActionListener {
+    private final String currentDir;
+    private final JFrame parent;
+    private final JTextField textField;
+
+    public SelectFileDisplayInTextField(String currentDir, JFrame parent, JTextField textField) {
+        this.currentDir = currentDir;
+        this.parent = parent;
+        this.textField = textField;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser jFileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "TIFF Images", "tif", "tiff", "TIF", "TIFF");
+        jFileChooser.setFileFilter(filter);
+        jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int returnValue = jFileChooser.showOpenDialog(parent);
+        if (returnValue == JFileChooser.APPROVE_OPTION){
+            File dir = jFileChooser.getSelectedFile();
+            if (dir != null) {
+                textField.setText(dir.getAbsolutePath());
+            }
+        }
+    }
+}
+
