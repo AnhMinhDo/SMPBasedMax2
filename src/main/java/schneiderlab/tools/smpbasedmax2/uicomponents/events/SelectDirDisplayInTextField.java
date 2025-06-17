6546d13a8e281240b61c1ef27e4022549937ac24
anhminh <ahd.minh@gmail.com>
2025-06-17 16:55:36 +0200

    feat: action event to open directory choserDialog and if successful, add full file path to the text field

---
+++ src/main/java/schneiderlab/tools/smpbasedmax2/uicomponents/events/SelectDirDisplayInTextField.java
@@ -0,0 +1,32 @@
+package schneiderlab.tools.smpbasedmax2.uicomponents.events;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class SelectDirDisplayInTextField implements ActionListener {
+    private final String currentDir;
+    private final JFrame parent;
+    private final JTextField textField;
+
+    public SelectDirDisplayInTextField(String currentDir, JFrame parent, JTextField textField) {
+        this.currentDir = currentDir;
+        this.parent = parent;
+        this.textField = textField;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnValue = jFileChooser.showOpenDialog(parent);
+        if (returnValue == JFileChooser.APPROVE_OPTION){
+            File dir = jFileChooser.getSelectedFile();
+            if (dir != null) {
+                textField.setText(dir.getAbsolutePath());
+            }
+        }
+    }
+}
