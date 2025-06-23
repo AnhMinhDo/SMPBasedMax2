fd84043e14c9bba340649bd9ede475dc7e4f169a
anhminh <ahd.minh@gmail.com>
2025-06-23 18:55:15 +0200

    feat: logic class to process single file without additional channels

---
+++ src/main/java/schneiderlab/tools/smpbasedmax2/services/HandleSingleFile.java
@@ -0,0 +1,109 @@
+package schneiderlab.tools.smpbasedmax2.services;
+
+import ij.ImagePlus;
+import ij.io.FileSaver;
+import schneiderlab.tools.smpbasedmax2.OutputTypeName;
+import schneiderlab.tools.smpbasedmax2.ZStackDirection;
+import schneiderlab.tools.smpbasedmax2.corecomputation.MaxIntensityProjection;
+import schneiderlab.tools.smpbasedmax2.corecomputation.SMP_MIP_Projection;
+import schneiderlab.tools.smpbasedmax2.corecomputation.SMProjection;
+import schneiderlab.tools.smpbasedmax2.helpersandutils.SmpBasedMaxUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+public class HandleSingleFile {
+
+    private ImagePlus inputImage;
+    private final int stiffness;
+    private final int filterSize;
+    private final ZStackDirection zStackDirection;
+    private final int offset;
+    private final int depth;
+    private float[] envMaxzValues;
+
+    private ImagePlus projectedImage;
+    private ImagePlus zMap;
+    private ImagePlus projectedSMPImage;
+    private ImagePlus smpZmap;
+    private ImagePlus projectedSMPMIPImage;
+    private ImagePlus smpMipZmap;
+
+    private boolean hasProcessed = false;
+
+    public HandleSingleFile(ImagePlus inputImage,
+                            ZStackDirection zStackDirection,
+                            int stiffness,
+                            int filterSize,
+                            int offset,
+                            int depth) {
+        this.inputImage = inputImage;
+        this.zStackDirection = zStackDirection;
+        this.stiffness = stiffness;
+        this.filterSize = filterSize;
+        this.offset = offset;
+        this.depth = depth;
+    }
+
+    public ImagePlus process(){
+        performProcessing();
+//        saveOutputToFile();
+        return depth==0 ? this.projectedSMPImage : projectedSMPMIPImage;
+    }
+
+//    public void setFilePath(String filePath) {
+//        this.filePath = filePath;
+//    }
+
+    public float[] getEnvMaxzValues() { return hasProcessed ? this.envMaxzValues : null;}
+
+    private void performProcessing(){
+        // create imagePlus object from filePath
+        inputImage = SmpBasedMaxUtil.preProcessInputImage(this.inputImage);
+        // ZProjecting MIP
+        MaxIntensityProjection projector = new MaxIntensityProjection(inputImage);
+        this.projectedImage = projector.doProjection();
+        this.zMap = projector.getZmap();
+        // ZProjecting SMP
+        SMProjection smProjector = new SMProjection(inputImage, zMap, stiffness, filterSize, zStackDirection, offset);
+        this.projectedSMPImage = smProjector.doSMProjection();
+        this.envMaxzValues = smProjector.getEnvMax();
+        hasProcessed = true;
+        this.smpZmap = smProjector.getSMPZmap();
+        // SMP-MIP if depth !=0
+        SMP_MIP_Projection smpMipProjector = new SMP_MIP_Projection(inputImage, smpZmap, depth, zStackDirection);
+        this.projectedSMPMIPImage = smpMipProjector.doProjection();
+        this.smpMipZmap = smpMipProjector.getZmap();
+    }
+
+//    private void saveOutputToFile(){
+//        try {
+//            // prepare the directory for output
+//            String resultDir = SmpBasedMaxUtil.createResultDir(filePath,
+//                    zStackDirection,
+//                    stiffness,
+//                    filterSize,
+//                    offset,
+//                    depth);
+//            String fileName = SmpBasedMaxUtil.extractFilename(filePath);
+//            // Save MIP projected Image and zMap
+//            FileSaver projectedImageTiff = new FileSaver(projectedImage);
+//            FileSaver zMapTiff = new FileSaver(zMap);
+//            projectedImageTiff.saveAsTiff(resultDir + File.separator +
+//                    fileName + "_MIP" + ".tif");
+//            zMapTiff.saveAsTiff(resultDir + File.separator +
+//                    fileName + "_MIP_zmap" + ".tif");
+//            // Save SMP projected image and zMap
+//            SmpBasedMaxUtil.savePostProcessImagePlus(this.projectedSMPImage, OutputTypeName.SMP,resultDir,fileName, stiffness, filterSize, offset, depth,true);
+//            SmpBasedMaxUtil.savePostProcessImagePlus(this.smpZmap, OutputTypeName.SMP_ZMAP,resultDir,fileName, stiffness, filterSize, offset, depth);
+//            // Save SMP depth-adjusted image and zMap
+//            if (depth != 0) {
+//                SmpBasedMaxUtil.savePostProcessImagePlus(this.projectedSMPMIPImage, OutputTypeName.SMPbasedMIP,resultDir,fileName, stiffness, filterSize, offset, depth,true);
+//                SmpBasedMaxUtil.savePostProcessImagePlus(this.smpMipZmap, OutputTypeName.SMPbasedMIP_ZMAP,resultDir,fileName, stiffness, filterSize, offset, depth);
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+}
