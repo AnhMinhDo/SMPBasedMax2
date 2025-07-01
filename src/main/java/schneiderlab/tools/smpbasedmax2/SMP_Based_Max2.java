bec87e5f99e0c99b6aab395b17808bbe2badf89b
anhminh <ahd.minh@gmail.com>
2025-07-01 14:59:37 +0200

    feat: add Main Model and Main Control for the application

--- src/main/java/schneiderlab/tools/smpbasedmax2/SMP_Based_Max2.java
+++ src/main/java/schneiderlab/tools/smpbasedmax2/SMP_Based_Max2.java
@@ -1,9 +1,16 @@
 package schneiderlab.tools.smpbasedmax2;
 
 import ij.plugin.PlugIn;
+import schneiderlab.tools.smpbasedmax2.controllers.MainController;
+import schneiderlab.tools.smpbasedmax2.models.MainModel;
 import schneiderlab.tools.smpbasedmax2.uicomponents.MainDialog;
 
 import javax.swing.*;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 public class SMP_Based_Max2 implements PlugIn {
 
@@ -23,10 +30,20 @@ public class SMP_Based_Max2 implements PlugIn {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        JFrame frame = new JFrame("Main Windows of Application");
+        JFrame frame = new JFrame("SMPTool Version 2");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
+        MainModel mainModel = new MainModel(ProcessingMode.SINGLE_FILE,
+                ThemeMode.LIGHT,
+                ZStackDirection.IN,
+                60,
+                30,
+                0,
+                0,
+                Paths.get(""),
+                Paths.get(""),
+                new HashSet<>(Collections.singletonList(OutputTypeName.SMP)));
         MainDialog form = new MainDialog(frame);
+        MainController mainController = new MainController(mainModel,form);
         frame.setContentPane(form.getContentPane());
         frame.pack();
         frame.setVisible(true);
