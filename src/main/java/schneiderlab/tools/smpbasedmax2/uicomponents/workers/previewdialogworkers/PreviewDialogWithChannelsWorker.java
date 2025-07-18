1be29b0fc3fb3b26293f28adf0ae59524d5e38bd
anhminh <ahd.minh@gmail.com>
2025-07-18 12:59:22 +0200

    feat: swing worker to perform projection with channels in the preview dialog

---
+++ src/main/java/schneiderlab/tools/smpbasedmax2/uicomponents/workers/previewdialogworkers/PreviewDialogWithChannelsWorker.java
@@ -0,0 +1,87 @@
+package schneiderlab.tools.smpbasedmax2.uicomponents.workers.previewdialogworkers;
+
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.ImageProcessor;
+import schneiderlab.tools.smpbasedmax2.ZStackDirection;
+import schneiderlab.tools.smpbasedmax2.services.HandleSingleFile;
+import schneiderlab.tools.smpbasedmax2.services.HandleSingleFileWithChannels;
+import schneiderlab.tools.smpbasedmax2.uicomponents.MainDialog;
+import schneiderlab.tools.smpbasedmax2.uicomponents.PreviewDialog;
+
+import javax.swing.*;
+import java.nio.file.Path;
+
+public class PreviewDialogWithChannelsWorker extends SwingWorker<Void, Void> {
+    private JTextField statusBar;
+    private JProgressBar progressBar;
+    private PreviewDialog previewDialogView;
+
+    private Path filePath;
+    private final int stiffness;
+    private final int filterSize;
+    private final ZStackDirection zStackDirection;
+    private final int offset;
+    private final int depth;
+    private float[] envMaxzValues;
+//    private final boolean hasManyChannels;
+    private final int referenceChannelIdx;
+
+    private ImagePlus projectedImage;
+    private ImagePlus zMap;
+    private ImagePlus projectedSMPImage;
+    private ImagePlus smpZmap;
+    private ImagePlus projectedSMPMIPImage;
+    private ImagePlus smpMipZmap;
+    private ImagePlus result;
+
+    public PreviewDialogWithChannelsWorker (JTextField statusBar,
+                                              JProgressBar progressBar,
+                                              PreviewDialog previewDialogView,
+                                              Path filePath,
+                                              int stiffness,
+                                              int filterSize,
+                                              ZStackDirection zStackDirection,
+                                              int offset,
+                                              int depth,
+                                              ImagePlus result,
+                                              int refChannelIdx) {
+        this.statusBar = statusBar;
+        this.progressBar = progressBar;
+        this.previewDialogView = previewDialogView;
+        this.filePath = filePath;
+        this.stiffness = stiffness;
+        this.filterSize = filterSize;
+        this.zStackDirection = zStackDirection;
+        this.offset = offset;
+        this.depth = depth;
+        this.result = result;
+        this.referenceChannelIdx=refChannelIdx;
+    }
+
+    @Override
+    protected Void doInBackground()  {
+        ImagePlus input = new ImagePlus(filePath.toAbsolutePath().toString());
+        HandleSingleFileWithChannels handleSingleFileWithChannels = new HandleSingleFileWithChannels(input,
+                zStackDirection,
+                stiffness,
+                filterSize,
+                offset,
+                depth,
+                referenceChannelIdx);
+        ImagePlus output = handleSingleFileWithChannels.process();
+//        ImageProcessor outputProcessor = output.getProcessor();
+//        result.setProcessor(outputProcessor);
+        result.setStack(output.getStack());
+        return null;
+    }
+
+    @Override
+    protected void done(){
+        progressBar.setValue(100);
+        statusBar.setText("Complete Processing");
+        statusBar.setToolTipText("Complete Processing");
+        result.updateAndRepaintWindow();
+
+    }
+}
